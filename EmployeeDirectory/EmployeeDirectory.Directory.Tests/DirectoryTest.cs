ff8a2e208def876ebd9d90d18e185df543227b8b
mamaddox <mariaddox@yahoo.com>
2019-08-02 15:16:24 -0400

    Add Directory.Tests

---
+++ EmployeeDirectory/EmployeeDirectory.Directory.Tests/DirectoryTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EmployeeDirectory.Directory.Tests
+{
+    [TestClass]
+    public class DirectoryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
